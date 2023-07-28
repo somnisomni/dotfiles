151c23c24a5d6a078f5dbbab591e61df9905bb02
somni <me@somni.one>
2023-07-29 05:11:54 +0900

    Add silent install of suggested app disabler script

---
+++ Windows/utils/Disable-SuggestedAppSilentInstall.cmd
@@ -0,0 +1,12 @@
+@ECHO OFF
+@COLOR F
+@TITLE Disable silent installation of suggested apps
+
+ECHO Disabling silent installation of suggested apps...
+ECHO.
+
+REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "SilentInstalledAppsEnabled" /t REG_DWORD /d 0 /f
+
+ECHO.
+ECHO Done!
+EXIT
