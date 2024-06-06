0cccf74386c25456cc35b665dad41b221e4261bf
somni <82272900+somnisomni@users.noreply.github.com>
2024-06-07 00:01:16 +0900

    Create Disable-WindowsAIFeatures.cmd

---
+++ Windows/utils/Disable-WindowsAIFeatures.cmd
@@ -0,0 +1,16 @@
+@ECHO OFF
+@COLOR F
+@TITLE Disable Windows AI features
+
+ECHO Disabling Windows AI features...
+ECHO.
+
+ECHO Windows Copilot
+REG ADD "HKLM\Software\Policies\Microsoft\Windows\WindowsCopilot" /v "TurnOffWindowsCopilot" /t REG_DWORD /d 1 /f
+
+ECHO AI data analysis (Windows Recall)
+REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsAI" /v "DisableAIDataAnalysis" /t REG_DWORD /d 1 /f
+
+ECHO.
+ECHO Done!
+EXIT
