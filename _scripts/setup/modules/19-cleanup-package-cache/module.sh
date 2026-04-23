efe3a2a7d977f0fccb65988a196894db4b910489
somni <me@somni.one>
2026-04-23 23:11:13 +0900

    Add package cache cleanup module

---
+++ _scripts/setup/modules/19-cleanup-package-cache/module.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+set -u
+
+PACKAGE_MANAGER=pacman
+
+##### MODULE PROCEDURE DEFINITIONS #####
+function check_before_run() {
+    if (command -v yay &> /dev/null); then
+        # If 'yay' is available, use it
+        PACKAGE_MANAGER=yay
+        return 0
+    fi
+
+    if (command -v $PACKAGE_MANAGER &> /dev/null); then
+        # If pacman or yay is available, good to go
+        return 0
+    fi
+
+    # Otherwise it's not good to go, well, maybe
+    return 0
+}
+
+function run() {
+    echo "Cleanup package caches using '$PACKAGE_MANAGER'..."
+    if [[ $PACKAGE_MANAGER = "pacman" ]]; then
+        yes | sudo $PACKAGE_MANAGER -Sc --noconfirm
+    else
+        yes | $PACKAGE_MANAGER -Sc --noconfirm
+    fi
+
+    return 0
+}
+
+function check_after_run() {
+    # Nothing to check, just pass 0
+    return 0
+}
+
+##### MODULE PROCEDURE EXECUTION #####
+check_before_run
+EXIT_CODE=$?
+
+if [[ $EXIT_CODE -eq 0 ]]; then
+    run
+    EXIT_CODE=$?
+
+    check_after_run
+    EXIT_CODE=$?
+fi
+
+exit $EXIT_CODE
