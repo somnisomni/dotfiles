6f30b36df6ae0fcb3f87bde6c17b0c26c25f5afd
somni <me@somni.one>
2026-04-26 15:33:28 +0900

    Add localectl setup module

---
+++ _scripts/setup/modules/20-localectl/module.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -u
+
+KEYBOARD_LAYOUT="kr"
+KEYBOARD_MODEL="pc104"
+KEYBOARD_VARIANT="kr104"
+KEYBOARD_OPTIONS="korean"
+
+##### MODULE PROCEDURE DEFINITIONS #####
+function check_before_run() {
+    # Nothing to check
+    return 0
+}
+
+function run() {
+    echo "Setting up X11 keyboard keymap: $KEYBOARD_MODEL $KEYBOARD_LAYOUT $KEYBOARD_VARIANT $KEYBOARD_OPTIONS"
+    localectl set-x11-keymap "$KEYBOARD_LAYOUT" "$KEYBOARD_MODEL" "$KEYBOARD_VARIANT" "$KEYBOARD_OPTIONS"
+
+    return 0
+}
+
+function check_after_run() {
+    # Check if the keymap was set correctly
+    localectl status | grep -qE "X11 Layout:\s+$KEYBOARD_LAYOUT" && \
+    localectl status | grep -qE "X11 Model:\s+$KEYBOARD_MODEL" && \
+    localectl status | grep -qE "X11 Variant:\s+$KEYBOARD_VARIANT" && \
+    localectl status | grep -qE "X11 Options:\s+$KEYBOARD_OPTIONS"
+
+    return $?
+}
+
+##### MODULE PROCEDURE EXECUTION #####
+check_before_run
+EXIT_CODE=$?
+
+if [[ $EXIT_CODE -eq 0 ]]; then
+    run
+    EXIT_CODE=$?
+
+    check_after_run
+    EXIT_CODE=$?
+fi
+
+exit $EXIT_CODE
