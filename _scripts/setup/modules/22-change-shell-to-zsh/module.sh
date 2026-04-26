18ccddb66d27404d847941179f12e9275f7bbc29
somni <me@somni.one>
2026-04-26 16:22:39 +0900

    Add chsh to zsh setup module

---
+++ _scripts/setup/modules/22-change-shell-to-zsh/module.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -u
+
+##### MODULE PROCEDURE DEFINITIONS #####
+function check_before_run() {
+    # Nothing to check
+    return 0
+}
+
+function run() {
+    echo "Updating user's shell to zsh..."
+    sudo chsh -s "$(which zsh)" "$USER"
+
+    echo "Link zsh configuration files to the root user's home directory..."
+    sudo ln -sf $HOME/.zshrc /root/.zshrc
+    sudo ln -sf $HOME/.p10k.zsh /root/.p10k.zsh
+
+    return 0
+}
+
+function check_after_run() {
+    # Check if the current user's shell is set to zsh
+    CURRENT_SHELL=$(getent passwd "$USER" | cut -d: -f7)
+
+    if [[ "$CURRENT_SHELL" != "$(which zsh)" ]]; then
+        return 1
+    fi
+
+    return 0
+}
+
+##### MODULE PROCEDURE EXECUTION #####
+check_before_run
+EXIT_CODE=$?
+
+if [[ $EXIT_CODE -eq 0 ]]; then
+    run
+    EXIT_CODE=$?
+
+    check_after_run
+    EXIT_CODE=$?
+fi
+
+exit $EXIT_CODE
